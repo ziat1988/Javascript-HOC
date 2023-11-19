facccb639c26e8aded21120e4115301e56b4ce79
dang <trinhdangnhat@gmail.com>
2023-11-19 22:39:54 +0100

    HOC for DRY

---
+++ main.js
@@ -0,0 +1,39 @@
+const inputTracker = (() => {
+  let form = null;
+  let dataStorage = null;
+
+  function wrapperTrack(fn) {
+    form = form ? form : getForm();
+    if (!form) {
+      return;
+    }
+
+    formName = form.getAttribute("name");
+    fn(formName);
+  }
+
+  function getForm() {
+    console.log("function getForm suppose expensive call");
+    return document.querySelector('[data-form-type="auto_save"]');
+  }
+
+  function trackInput() {
+    wrapperTrack(function (formName) {
+      console.log("this is from input :", formName);
+    });
+  }
+
+  function trackTextArea() {
+    wrapperTrack(function (form) {
+      console.log("this is from text area");
+    });
+  }
+
+  return {
+    trackInput,
+    trackTextArea,
+  };
+})();
+
+inputTracker.trackInput();
+inputTracker.trackTextArea();
